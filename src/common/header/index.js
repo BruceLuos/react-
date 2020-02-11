c085111700a0703762a6fa2c6006e0b9381f8d5a
BruceLuo <295448382@qq.com>
2020-02-11 23:26:42 +0800

    '使用react-transition-group进行对搜索框动画的实现'

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,25 +1,66 @@
 import React, { Component } from "react";
-import { HeaderWrapper,Logo,Navi,NaviItem,NaviInput,Addition,Button} from "./style";
+import { CSSTransition } from "react-transition-group";
+import {
+  HeaderWrapper,
+  Logo,
+  Navi,
+  NaviItem,
+  NaviInput,
+  Addition,
+  Button,
+  SearchWrapper
+} from "./style";
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      focurs: false
+    };
+    this.handleFocus = this.handleFocus.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
+  }
   render() {
     return (
       <HeaderWrapper>
         <Logo href="/"></Logo>
         <Navi>
-            <NaviItem className='left active'>首页</NaviItem>
-            <NaviItem className='left'>下载APP</NaviItem>
-            <NaviItem className='right'>登录</NaviItem>
-            <NaviItem className="right beta"></NaviItem>
-            <NaviItem className='right'>Aa</NaviItem>
-            <NaviInput></NaviInput>
+          <NaviItem className="left active">首页</NaviItem>
+          <NaviItem className="left">下载APP</NaviItem>
+          <NaviItem className="right">登录</NaviItem>
+          <NaviItem className="right beta"></NaviItem>
+          <NaviItem className="right">Aa</NaviItem>
+          <SearchWrapper>
+            <i className={this.state.focurs ? "search focurs" : "search"}></i>
+            <CSSTransition
+              in={this.state.focurs}
+              timeout={200}
+              classNames="slide"
+            >
+              <NaviInput
+                className={this.state.focurs ? "focurs" : ""}
+                onFocus={this.handleFocus}
+                onBlur={this.handleBlur}
+              ></NaviInput>
+            </CSSTransition>
+          </SearchWrapper>
         </Navi>
         <Addition>
-            <Button>注册</Button>
-            <Button>写文章</Button>
+          <Button>注册</Button>
+          <Button className="article">写文章</Button>
         </Addition>
       </HeaderWrapper>
     );
+  }
+  handleFocus() {
+    this.setState({
+      focurs: true
+    });
+  }
+  handleBlur() {
+    this.setState({
+      focurs: false
+    });
   }
 }
 export default Header;
--- src/common/header/style.js
+++ src/common/header/style.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
+
 import LogoUrl from "../../statics/logo.png";
-import BetaUrl from '../../statics/nav-beta.png'
+import BetaUrl from "../../statics/nav-beta.png";
 export const HeaderWrapper = styled.div`
   position: relative;
   height: 56px;
@@ -17,8 +18,7 @@ export const Logo = styled.a`
 export const Navi = styled.div`
   width: 960px;
   height: 100%;
-  padding-left:70px;
-  box-sizing:border-box;
+  box-sizing: border-box;
   margin: 0 auto;
 `;
 export const NaviItem = styled.div`
@@ -36,47 +36,82 @@ export const NaviItem = styled.div`
   &.active {
     color: #ea6f5a;
   }
-  &.beta{
-      background:url(${BetaUrl}) no-repeat;
-      background-size:contain;
-      height: 25px;
-      width: 55px;
-      margin-top: 15px;
-      
+  &.beta {
+    background: url(${BetaUrl}) no-repeat;
+    background-size: contain;
+    height: 25px;
+    width: 55px;
+    margin-top: 15px;
+  }
+`;
+export const SearchWrapper = styled.div`
+  position: relative;
+  float: left;
+  .slide-enter {
+    transition: all 0.2s ease-in;
+  }
+  .slide-enter-active {
+    width: 240px;
+  }
+  .slide-exit {
+    transition: all 0.2s ease-out;
+  }
+  .slide-exit-active {
+    width: 200px;
+  }
+  .search {
+    position: absolute;
+    top: 12px;
+    right: 6px;
+    background: pink;
+    height: 30px;
+    width: 30px;
+    border-radius: 50%;
+    &.focurs {
+      background: grey;
+    }
   }
 `;
 export const NaviInput = styled.input.attrs({
   placeholder: "搜索"
 })`
-    width:160px;
-    height:38px;
-    padding: 0 20px;
-    box-sizing:border-box;
-    margin-top:9px;
-    margin-left:20px;
-    border-radius:19px;
-    border:none;
-    border-line:none;
-    background:#eee;
-    font-size:15px;
-    &::placeholder{
-        color:#999;
-    }
+  width: 200px;
+  height: 38px;
+  padding: 0 35px 0 15px;
+  box-sizing: border-box;
+  margin-top: 9px;
+  margin-left: 20px;
+  border-radius: 19px;
+  border: none;
+  outline: none;
+  background: #eee;
+  font-size: 15px;
+  &::placeholder {
+    color: #999;
+  }
+  &.focurs {
+    width: 240px;
+  }
 `;
 export const Addition = styled.div`
-    position:absolute;
-    top:0;
-    right:0;
-    height:56px;
-    background:red;
-`
+  position: absolute;
+  top: 0;
+  right: 60px;
+  height: 56px;
+`;
 export const Button = styled.div`
-    float:left;
-    border-radius:19px;
-    background:white;
-    margin-top:9px;
-    padding:0 20px;
-    margin-right: 20px;
-    line-height:38px;
-    border:1px solid 
-`
+  float: left;
+  border-radius: 19px;
+  background: white;
+  margin-top: 9px;
+  padding: 0 20px;
+  margin-right: 20px;
+  line-height: 38px;
+  border: 1px solid rgba(236, 97, 73, 0.7);
+  font-size: 15px;
+  color: #ea6f5a;
+  &.article {
+    color: white;
+    background: #ea6f5a;
+  }
+`;
