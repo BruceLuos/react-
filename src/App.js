3096ed7657e067de3dbda54560c7d34bdae1822d
BruceLuo <295448382@qq.com>
2020-02-15 23:46:05 +0800

    '使用react-router-dom进行路由的配置'

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
+import {BrowserRouter,Route} from "react-router-dom"
 import Header from "./common/header";
 import store from "./store";
 import {Provider} from "react-redux";
 import { GlobalIcon } from "./statics/iconfont/iconfont";
+import Home from "./pages/home";
+import Detail from "./pages/detail";
 function App() {
   return (
     <div className="App">
       <Provider store={store}>
         <Header></Header>
         <GlobalIcon></GlobalIcon>
+        <BrowserRouter>
+          <div>
+            <Route path='/' exact component={Home}></Route>
+            <Route path='/detail' exact component={Detail}></Route>
+          </div>
+        </BrowserRouter>
       </Provider>
     </div>
   );
